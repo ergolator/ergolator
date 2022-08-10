6b1a5ca0d0885654c68dcf9e7d1c45afc191ee04
Mohammed <mohammed.ramadan1474@gmail.com>
2022-08-10 21:53:34 +0200

    adds calculator grid

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 "use strict";
+
+const rulaContainer = document.querySelector(".rula-container");
+
+rulaContainer.addEventListener("click", () => {
+  window.location.href = "./rula-page.html";
+});
